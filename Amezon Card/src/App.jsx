b4a85ae98de084988a8d98db4946a268d3f77f09
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2024-12-23 16:28:00 +0530

    Remove Card and Navbar components; update App component structure and add new dependencies

--- Amezon Card/src/App.jsx
+++ Amezon Card/src/App.jsx
@@ -1,13 +1,5 @@
-import Navbar from "./Components/Navbar";
-import Product from "./Components/Product";
-
 const App = () => {
-  return (
-    <div className="bg-zinc-800 min-h-screen w-screen overflow-hidden text-white p-10">
-      {/* <Navbar /> */}
-      <Product />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
